dacbcda1f4143fec0f853468032a65f946258534
tahiamubin <tahia.mubin1@gmail.com>
2026-07-11 21:22:12 +0600

    features

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,6 +1,12 @@
 import { guides } from "@/data/guides";
 import ExploreCard from "../components/ExploreCrad";
 
-export default function ExplorePage () {
-    return <ExploreCard guides = {guides}/>
+export default function ExplorePage() {
+  return (
+    <div className="container mx-auto gap-4 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+      {guides.map((guide) => (
+        <ExploreCard  key={guide.id} guide={guide} />
+      ))}
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,28 @@ import FAQ from "./components/FAQ";
 import Newsletter from "./components/Newsletter";
 import ProductivityTips from "./components/ProductivityTips";
 import TestimonialsSimple from "./components/Testimonials";
-
+import { guides } from "@/data/guides";
+import ExploreCard from "./components/ExploreCrad";
 
 export default function Home() {
   return (
     <div>
-     <Hero/>
-      <TestimonialsSimple/>
-        <ProductivityTips/>
-        <Newsletter/>
-        <FAQ/>
+      <Hero />
+      <div>
+        <h2 className="text-4xl md:text-5xl font-bold text-black mb-4 text-center pt-10 pb-10">
+          Feature <span style={{ color: "#7283ff" }}>Guides</span>
+        </h2>
+        <div className="container mx-auto gap-4 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+          {guides.slice(0, 4).map((guide) => (
+            <ExploreCard key={guide.id} guide={guide} />
+          ))}
+        </div>
+      </div>
+
+      <TestimonialsSimple />
+      <ProductivityTips />
+      <Newsletter />
+      <FAQ />
     </div>
   );
 }
